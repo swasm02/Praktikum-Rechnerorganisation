b790d1d91985f7d2a3b675b97abbee6983e6f2cf
Stefan Wasmer <stefan.wasmer@tu-ilmenau.de>
2023-05-23 18:38:23 +0200

    first task
    simple addition of two registers
    flag check

---
+++ A1.asm
@@ -0,0 +1,11 @@
+;Es folgen die Beispieldaten, bitte entsprechende Zeilen einkommentieren
+; include "data1_1.inc"
+; include "data1_2.inc"
+; include "data1_3.inc"
+; include "data1_4.inc"
+
+name "Aufgabe 1"
+
+MOV BL,AL
+ADD BL,AH
+hlt  ;                                         
